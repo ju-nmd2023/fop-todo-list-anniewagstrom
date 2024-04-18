5bb70f99eb6ef2b5e0e2d558c4731cd93c0f4b08
anniewagstrom <waan23qp@student.ju.se>
2024-04-18 23:42:46 +0200

    final code

---
+++ todolist.js
@@ -0,0 +1,39 @@
+const inputText = document.getElementById("input-text");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputText.value === '') {
+        alert("Please write some text!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputText.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputText.value = '';
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("check");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function viewTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+viewTask();
